53d69a073791b6c3eaa156cd36714ab1a0a2a6b8
Muhammad Taezeem Tariq <101944712+taezeem@users.noreply.github.com>
2026-01-04 19:43:59 +0530

    Update hackoAI.js

--- netlify/functions/hackoAI.js
+++ netlify/functions/hackoAI.js
@@ -77,7 +77,7 @@ Rules:
             },
             { role: "user", content: text }
           ],
-          max_tokens: 1100
+          max_tokens: 2500
         })
       }
     );
@@ -95,6 +95,7 @@ Rules:
     };
   }
 }
+
 
 
 
