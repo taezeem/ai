10bc877fd722557a21ded4ae2dbe48119d2c042c
Muhammad Taezeem Tariq <101944712+taezeem@users.noreply.github.com>
2026-01-20 15:12:56 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,24 +1,21 @@
-const CACHE = 'hackoai-v1';
+// sw.js
+const CACHE_NAME = 'hackoai-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  // Add other assets you want to cache
+];
 
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE).then(cache => 
-      cache.addAll(['/'])
-    )
-  );
-  self.skipWaiting();
-});
-
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys => 
-      Promise.all(keys.map(k => k !== CACHE && caches.delete(k)))
-    )
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
   );
 });
